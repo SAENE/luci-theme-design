e82f01b1be7d4da86935407c590b56e682cc2135
relife <verticle@foxmail.com>
2023-02-01 14:22:44 +0800

    update

--- htdocs/luci-static/design/js/script.js
+++ htdocs/luci-static/design/js/script.js
@@ -21,7 +21,7 @@
 
     // 修复某些插件导致在https下env(safe-area-inset-bottom)为0的情况
     var url = self.location.href; 
-    if (url.indexOf("openclash") != -1 ) {
+    if ((/(iPhone|iPad|iPod|iOS|Mac|Macintosh)/i.test(navigator.userAgent)) && url.indexOf("openclash") != -1 ) {
         var oMeta = document.createElement('meta');
         oMeta.content = 'width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0,viewport-fit=cover';
         oMeta.name = 'viewport';
