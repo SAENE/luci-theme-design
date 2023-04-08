d1da05ced6792b8de6d52c8a628cab20e1be6e75
gngpp <gngppz@gmail.com>
2023-04-08 19:22:40 +0800

    refactor: refactor resource loading logic

--- dev/design.js
+++ dev/design.js
@@ -240,22 +240,7 @@ XHR.running = function()
 	return !!(XHR._r && XHR._i);
 }
 
-(function ($) {
-  const global = $('head #global-scroll');
-  const isMobile = /phone|pad|pod|iPhone|iPod|ios|iOS|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone/i.test(navigator.userAgent);
-
-  // Fixed scrollbar styles for browsers on different platforms
-  function settingGlobalScroll() {
-    if (!isMobile && global.length === 0) {
-      const style = document.createElement('style');
-      style.id = 'global-scroll';
-      style.textContent = `*::-webkit-scrollbar { width: 4px;height: 4px; } *::-webkit-scrollbar-thumb { background: var(--scrollbarColor); border-radius: 2px; }`;
-      $('head').append(style);
-    } else if (isMobile && global.length > 0) {
-      global.remove();
-    }
-  }
-
+;(function ($) {
   // Fixed status realtime table overflow style
   function settingsStatusRealtimeOverflow() {
     if (self.location.pathname.includes("status/realtime")) {
@@ -275,13 +260,7 @@ XHR.running = function()
   }
 
   $(document).ready(() => {
-    settingGlobalScroll();
     settingsStatusRealtimeOverflow();
   });
 
-  $(window).on('resize', () => {
-    settingGlobalScroll();
-  });
-
-  $(window)
 })(jQuery);
